e7cf1202bacb7d6072d780b0187e700f65b2aff3
Haris Khan <haris.a.khan@stonybrook.edu>
2022-01-25 13:14:26 -0500

    Solution to question 450

--- java/450.Delete-Node-in-BST.java
+++ java/450.Delete-Node-in-BST.java
@@ -1,5 +1,31 @@
-       }
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){return null;}
+        
+        if(key<root.val){
+            root.left =deleteNode(root.left,key);
+        }else if(key> root.val){
+           root.right =  deleteNode(root.right,key);
+        }
+        else {
+            if(root.left ==null){
+            TreeNode right = root.right;
+            root=null;
+            return right;
+        }if(root.right == null){
+                TreeNode left = root.left;
+            root=null;
+            return left;
+            }
+            root.val = succesor(root.right);
+            root.right =  deleteNode(root.right,root.val);
+        }
+        return root;
+    }
+    public int succesor(TreeNode t){
+        if(t.left == null){
+            return t.val;
+        }
         return succesor(t.left);
     }
-
-
+}
